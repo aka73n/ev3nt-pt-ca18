f784183df4890f534135d78917186eee5477bd27
aka73n <akasaya7@gmail.com>
2026-07-17 13:10:59 +0900

    Update accordion.tsx

--- web/src/components/ui/accordion.tsx
+++ web/src/components/ui/accordion.tsx
@@ -27,7 +27,7 @@ const AccordionTrigger = ({
       {...props}
     >
       {children}
-      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-150" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 );
